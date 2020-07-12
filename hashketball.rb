f2f47c2026f9437aa5935a9fc18dcb9805367e63
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-12 19:37:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ game_hash = {
   :home =>[
     :team_name => "Brooklyn Nets",
     :colors=>["Black", "White"],
-    :players=>[{:player_name =>},{:number =>},{:shoe =>},{:points =>},{:rebounds =>},{:assists =>},{:steals},{}]
+    :players=>[{:player_name =>},{:number =>},{:shoe =>},{:points =>},{:rebounds =>},{:assists =>},{:steals},{:blocks =>},{}]
     ],
   :away =>[
     :team_name => "Charlotte Hornets",
