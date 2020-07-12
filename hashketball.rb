a1765cc5efa77a0c133f31b85900b7c007587073
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-12 19:43:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,11 +3,29 @@ game_hash = {
   :home =>[
     :team_name => "Brooklyn Nets",
     :colors=>["Black", "White"],
-    :players=>[{:player_name =>"Alan Anderson",},{:number =>},{:shoe =>},{:points =>},{:rebounds =>},{:assists =>},{:steals},{:blocks =>},{:slam_dunks =>}]
-    ],
+    :players=>[
+      {:player_name =>"Alan Anderson","Reggie Evans",""},
+      {:number =>},
+      {:shoe =>},
+      {:points =>},
+      {:rebounds =>},
+      {:assists =>},
+      {:steals},
+      {:blocks =>},
+      {:slam_dunks =>}
+      ]],
   :away =>[
     :team_name => "Charlotte Hornets",
     :colors=>["Turquoise", "Purple"],
-    :players=>[{:player_name =>},{:number =>},{:shoe =>},{:points =>},{:rebounds =>},{:assists =>},{:steals},{:blocks =>},{:slam_dunks =>}]
-    ]
+    :players=>[
+      {:player_name =>},
+      {:number =>},
+      {:shoe =>},
+      {:points =>},
+      {:rebounds =>},
+      {:assists =>},
+      {:steals},
+      {:blocks =>},
+      {:slam_dunks =>}
+      ]]
 }
